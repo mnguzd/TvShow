3fe0e463c1120bccf89fb335c9c88502076422a6
mnguzd <roman.kioroglo@gmail.com>
2020-09-10 12:30:48 +0300

    Created application

---
+++ FilmsApp/Data/TvShowsContext.cs
@@ -0,0 +1,15 @@
+﻿using FilmsApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmsApp.Data
+{
+    public class TvShowsContext : DbContext
+    {
+        public TvShowsContext(DbContextOptions<TvShowsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TvShow> TvShow { get; set; }
+    }
+}
---
+++ FilmsApp/Models/TvShow.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmsApp.Models
+{
+    public class TvShow
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        public decimal Rating { get; set; }
+
+        [Required]
+        [DataType(DataType.Url)]
+        [Display(Name ="Imbd Link")]
+        public string ImbdUrl { get; set; }
+
+        [Required]
+        [DataType(DataType.ImageUrl)]
+        [Display(Name ="Poster")]
+        public string ImageUrl { get; set; }
+    }
+}
